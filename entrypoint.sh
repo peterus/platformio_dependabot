d04cb7cec933dd8276093f4a7ec460b9ef15af41
Oleksii Kutuzov <oleksii.kutuzov@icloud.com>
2024-06-30 10:16:41 +0200

    Configure bot credentials

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -l
 
 git config --global --add safe.directory /github/workspace
+git config --global user.name 'github-actions[bot]'
+git config --global user.email 'github-actions[bot]@users.noreply.github.com'
 
 export PYTHONPATH=$PYTHONPATH:/app
 python3 -m platformio_dependabot
